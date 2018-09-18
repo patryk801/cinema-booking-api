05637312851de722354e074090feba0155203bb3
Patryk Ruszaj <patryk801@aol.pl>
2018-09-18 23:22:52 +0200

    Added post user action.

--- src/AppBundle/Controller/UsersController.php
+++ src/AppBundle/Controller/UsersController.php
@@ -2,10 +2,15 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
+use AppBundle\Exception\ValidationException;
 use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 /**
  * @Security("is_anonymous() or is_authenticated()")
@@ -19,5 +24,24 @@ class UsersController extends AbstractController
     {
         $this->passwordEncoder = $passwordEncoder;
         $this->jwtEncoder = $jwtEncoder;
+    }
+
+    /**
+     * @Rest\View(statusCode=201)
+     * @Rest\NoRoute()
+     * @ParamConverter("user", converter="fos_rest.request_body", options={"deserializationContext" = {"groups"={"Deserialize"}}})
+     */
+    public function postUserAction(User $user, ConstraintViolationListInterface $validationErrors)
+    {
+        if(count($validationErrors) > 0) throw new ValidationException($validationErrors);
+
+        $user->setPassword($this->passwordEncoder->encodePassword($user, $user->getPassword()));
+        $user->setRoles([User::ROlE_USER]);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return $user;
     }
 }
